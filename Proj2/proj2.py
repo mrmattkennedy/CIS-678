ada5b2b58c1f7028d315a0d3eefc3313cfa7e0e1
Matthew Kennedy <35405371+mrmattkennedy@users.noreply.github.com>
2019-02-12 15:01:57 -0500

    Reads in words and counts them in each document type

--- Proj2/proj2.py
+++ Proj2/proj2.py
@@ -1,60 +1,77 @@
 #!/usr/bin/env python
 import time
 from itertools import chain
+from operator import itemgetter
 
-atheism_total = 480
-graphics_total = 584
-mswindows_total = 572
-pc_total = 590
-mac_total = 578
-xwindows_total = 593
-forsale_total = 585
-autos_total = 594
-motorcycles_total = 598
-baseball_total = 597
-hockey_total = 600
-cryptology_total = 595
-electronics_total = 591
-medicine_total = 594
-space_total = 593
-christianity_total = 598
-guns_total = 545
-mideastpolitics_total = 564
-politics_total = 465
-religion_total = 377
+totals_list = [["atheism", 480],
+               ["graphics", 584],
+               ["mswindows", 572],
+               ["pc", 590],
+               ["mac", 578],
+               ["windows", 593],
+               ["forsale", 585],
+               ["autos", 594],
+               ["motorcycles", 598],
+               ["baseball", 597],
+               ["hockey", 600],
+               ["cryptology", 595],
+               ["electronics", 591],
+               ["medicine", 594],
+               ["space", 593],
+               ["christianity", 598],
+               ["guns", 545],
+               ["mideastpolitics", 564],
+               ["politics", 465],
+               ["religion", 377]]
 total = 11293
 
-def get_word_counts():
+def save_word_counts():
     training_data = open("forumTraining.data", "r")
     documents = [line for line in training_data.readlines()]
     words = [document.split() for document in documents]
 
     word_count = []
-    for i in range(0, 480):
-        print(i)
-        for word in words[i]:
-            if word is not "to" and word is not "it" and word is not "of":
-                if len(word_count) == 0:
-                    temp_list = [word, 1]
-                    word_count.append(temp_list)
-                    continue
-                else:
-                    if any(word in current_word for current_word in word_count):
-                        index = [i for i, lst in enumerate(word_count) if word in lst][0] 
-                        word_count[index][1] = word_count[index][1] + 1
-                        continue
+    word_count_file = open("word_counts.txt", "w")
+    start_index = 0
 
-                    temp_list = [word, 1]
-                    word_count.append(temp_list)
+    for topic in range(0, len(totals_list)):
+        word_count_file.write(totals_list[topic][0] + "\n")
+        word_count.clear()
+        print(totals_list[topic][0])
+        for i in range(start_index, start_index + totals_list[topic][1]):
+            for word in words[i]:
+                if word is not "to" and word is not "it" and word is not "of":
+                    if len(word_count) == 0:
+                        temp_list = [word, 1]
+                        word_count.append(temp_list)
+                        continue
+                    else:
+                        if any(word in current_word for current_word in word_count):
+                            index = [i for i, lst in enumerate(word_count) if word in lst][0] 
+                            word_count[index][1] = word_count[index][1] + 1
+                            continue
+
+                        temp_list = [word, 1]
+                        word_count.append(temp_list)
+
+        start_index = start_index + totals_list[topic][1]
+       
+        for word in word_count:
+            word_count_file.write(str(word[0]) + " " + str(word[1]) + "\n")
+       
+        
 
     print(len(word_count))
     time.sleep(2)
 
-    """
-    for word in word_count:
-        if (word[1] > 1):
-            print(word[0] + ": " + str(word[1]))
-    """
+
+    word_count_file.close()
     training_data.close()
 
-get_word_counts()
+def load_word_counts():
+    training_data = open("word_counts.txt", "r")
+    words = [word.split() for word in training_data.readlines()]
+    training_data.close()
+
+#save_word_counts()
+load_word_counts()
