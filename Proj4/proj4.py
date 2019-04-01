c998ac70354fbd633677aaca090166bc0b8e0610
mrmattkennedy <kennemat@mail.gvsu.edu>
2019-04-01 13:32:02 -0400

    Project 4 first iteration working

--- Proj4/proj4.py
+++ Proj4/proj4.py
@@ -1,21 +1,76 @@
+from collections import OrderedDict
+import math
 import random
+import time
 
 class ANN:
-    def __init__(self, training_set):
+    def __init__(self, training_set, test_set):
         with open(training_set, "r") as file:
             self.training_set = file.readlines()
-        self.perceptron = [{i:random.uniform(-0.01, 0.01) for i in range(len(self.training_set[0].split()) - 1)} for j in range(10)]
-        self.outputs = [0 for i in range(10)]
+        with open(test_set, "r") as file:
+            self.test_set = file.readlines()
+
+        self.num_inputs = len(self.training_set[0].split()[:-1])
+        self.num_outputs = 10
+        self.num_hidden = math.ceil((2/3) * (self.num_inputs + self.num_outputs))
+        #self.input_hidden = {j:[random.uniform(-0.01, 0.01) for i in range(self.num_inputs)] for j in range(self.num_hidden)}
+        #self.hidden_output = 
+        self.input_hidden = [[random.uniform(-0.01, 0.01) for i in range(self.num_hidden)] for j in range(self.num_inputs)]
+        self.hidden_output = [[random.uniform(-0.01, 0.01) for i in range(self.num_outputs)] for j in range(self.num_hidden)]
+        """
+        for val in self.perceptron:
+            print(str(val) + "," + str(self.perceptron[val]))
+            time.sleep(1)
+        """
+        self.hidden_values = [0 for i in range(self.num_hidden)]
+        self.outputs = [0 for i in range(self.num_outputs)]
+        self.target = 1
+        self.learning_rate = 0.01
+        self.total_error = -999
+
     def feed_forward(self):
-        current_values = [int(i) for i in self.training_set[0].split()[:-1]]
-        for dictionary in range(len(self.perceptron)):
-            for value in range(len(self.perceptron[dictionary])):
-                self.outputs[dictionary] += self.perceptron[dictionary][value] * current_values[value]
+        for data in range(500):
+            current_values = [float(i)/16 for i in self.training_set[data].split()[:-1]]
+            current_values = [-1 if i == 0.0 else i for i in current_values]
+#            print(current_values)
+            for val_input in range(len(self.input_hidden)):
+                for weight in range(len(self.input_hidden[val_input])):
+                    self.hidden_values[weight] += self.input_hidden[val_input][weight] * current_values[val_input]
+ 
+            self.hidden_values = [1/(1+math.exp(-i)) for i in self.hidden_values]
+            for val_hidden in range(len(self.hidden_values)):
+                for weight in range(len(self.hidden_output[val_hidden])):
+                    self.outputs[weight] += self.hidden_output[val_hidden][weight] * self.hidden_values[val_hidden]
 
-#        print([round(i, 3) for i in self.outputs])
-        error = 0.5*sum([1-i for i in self.outputs]) ** 2
-        print(error)
+            self.back_propogate(current_values)
+            print(str(self.total_error - sum([0.5 * ((1 - i) ** 2) for i in self.outputs])))
+            if self.total_error - sum([0.5 * ((1 - i) ** 2) for i in self.outputs]) == 0.0:
+                return
+            self.total_error = sum([0.5 * ((1 - i) ** 2) for i in self.outputs])
 
-for _ in range(15):
-    ann_ex = ANN("digits-training.data")
+    def back_propogate(self, current_values):
+        output_errors = [i*(1-i)*(self.target-i) for i in self.outputs]
+        #hidden = h(1-h)sum(weight * error) for each output unit
+        #0 = hidden to output 0, 1, 2...n
+        hidden_errors = [h * (1 - h) * sum(j) for j in self.hidden_output for h in self.hidden_values]
+#        print("Hidden errors are " + str(hidden_errors))
+        self.learn(current_values, output_errors, hidden_errors)
+
+    def learn(self, current_values, output_errors, hidden_errors):
+        #w_ij = w_ij + learningrate * error(j) * value(j)
+        #wnew = old + 0.5(learning_rate)(current_val)(1+Y)(1-Y)(Y-T
+        for val_hidden in range(len(self.hidden_values)):
+            for weight in range(len(self.hidden_output[val_hidden])):
+                self.hidden_output[val_hidden][weight] += (0.5 * self.learning_rate) * (self.hidden_values[val_hidden]) * (1+output_errors[weight]) * (1-output_errors[weight]) * (output_errors[weight] - self.target)
+
+        for val_input in range(len(self.input_hidden)):
+            for weight in range(len(self.input_hidden[val_input])):
+                #self.input_hidden[val_input][weight] -= self.learning_rate * hidden_errors[weight] * current_values[val_hidden]
+                self.input_hidden[val_input][weight] += (0.5 * self.learning_rate) * (current_values[val_input]) * (1+hidden_errors[weight]) * (1-hidden_errors[weight]) * (hidden_errors[weight] - self.target)
+    def predict(self):
+        return
+
+for _ in range(1):
+    ann_ex = ANN("digits-training.data", "digits-test.data")
+
     ann_ex.feed_forward()
